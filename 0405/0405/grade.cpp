c53971accf37535d19c9f473061943647308d734
YakumoRan <37825675+dudals191@users.noreply.github.com>
2018-04-05 15:48:59 +0900

    4/5 파일추가

---
+++ 0405/0405/grade.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+	int score;
+	char grade;
+
+	printf("성적을 입력하시오 : ");
+	scanf_s("%d", &score);
+
+	if (score >= 90) grade = 'A';
+	else if (score >= 80) grade = 'B';
+	else if (score >= 70) grade = 'C';
+	else if (score >= 60) grade = 'D';
+	else grade = 'F';
+
+	printf("학점 %c \n", grade);
+}
