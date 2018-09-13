745583fd042e85246ab5d7373e842e0327caa736
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-09-13 15:39:26 +0900

    malloc

---
+++ 0913/0913/malloc1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+	char *pc;
+
+	pc = (char *)malloc(1 * sizeof(char));
+	if (pc == NULL) {
+		printf("메모리 할당 오류\n");
+		exit(1);
+	}
+
+	*pc = 'a';
+	printf("%c \n", *pc);
+	free(pc);
+}
