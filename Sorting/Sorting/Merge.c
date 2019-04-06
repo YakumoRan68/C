9f892262dce1931615bcab9cd31400b143a81f9e
YakumoRan68 <37825675+YakumoRan68@users.noreply.github.com>
2019-04-06 18:41:29 +0900

    Update Merge.c

--- Sorting/Sorting/Merge.c
+++ Sorting/Sorting/Merge.c
@@ -9,14 +9,15 @@ void MergeArray(int list[], int left, int mid, int right) {
 
 	while (index <= mid && low <= right) 
 		MEMORY[high++] = list[index] > list[low] ? list[low++] : list[index++];
+	//교수님이 짜신 부분을 약간 최적화
 
-	if (index > mid) {
+	if (index > mid) 
 		for (int i = low; i <= right; i++)
 			MEMORY[high++] = list[i];
-	} else {
+	else 
 		for (int i = index; i <= mid; i++)
 			MEMORY[high++] = list[i];
-	}
+	
 
 	for (int i = left; i <= right; i++) 
 		list[i] = MEMORY[i];
@@ -29,7 +30,7 @@ void MergeArray(int list[], int left, int mid, int right) {
 }
 
 void MergeSort(int list[], int left, int right) { //재귀문 사용
-	if (left >= right) return;
+	if (left >= right) return; //블록 탈출을 빨리 하는게 더 속도가 빠름.
 
 	int mid = (left + right) / 2;
 	MergeSort(list, left, mid);
@@ -38,53 +39,20 @@ void MergeSort(int list[], int left, int right) { //재귀문 사용
 	
 }
 
-void mergesort(int list[]) { //반복문 사용(Bottom-Up 알고리즘)
-	int i, j, l1, l2, h1, h2, k;
+#define MIN(x, y) x < y ? x : y;
+void mergesort(int list[]) { // 반복문 사용(Bottom-Up 알고리즘)
+	for (int size = 1; size <= ARRAYSIZE - 1; size = 2 * size) { //1칸의 배열부터 시작해서 크기를 2배씩 늘린다
+		for (int i = 0; i < ARRAYSIZE - 1; i += 2 * size) {
+			int left = i;
+			int mid = i + size - 1;
+			int right = MIN(i + 2 * size - 1, ARRAYSIZE - 1);
 
-	for (int size = 1; size < ARRAYSIZE; size = size * 2) {
-		l1 = 0;
-		k = 0;
-		while (l1 + size < ARRAYSIZE) {
-			h1 = l1 + size - 1;
-			l2 = h1 + 1;
-			h2 = l2 + size - 1;
-			
-			if (h2 >= ARRAYSIZE)
-				h2 = ARRAYSIZE - 1;
-
-			i = l1;
-			j = l2;
-
-			while (i <= h1 && j <= h2) {
-				if (list[i] <= list[j])
-					MEMORY[k++] = list[i++];
-				else
-					MEMORY[k++] = list[j++];
-			}
-
-			while (i <= h1)
-				MEMORY[k++] = list[i++];
-			while (j <= h2)
-				MEMORY[k++] = list[j++];
-
-			l1 = h2 + 1;
-
-#if SHOW_PROGRESS
-			PrintArray(list);
-			PrintArray(MEMORY);
-#endif
-			count++;
+			MergeArray(list, left, mid, right);
 		}
-
-		for (int i = l1; k < ARRAYSIZE; i++)
-			MEMORY[k++] = list[i];
-
-		for (int i = 0; i < ARRAYSIZE; i++)
-			list[i] = MEMORY[i];
 	}
 }
 
 void Merge(int* ToSort) {
-	MergeSort(ToSort, 0, ARRAYSIZE - 1);
-	//mergesort(ToSort);
+	//MergeSort(ToSort, 0, ARRAYSIZE - 1);
+	mergesort(ToSort);
 }
