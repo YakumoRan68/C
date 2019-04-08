8fff43780fbc57bb340a5c61fdec2fd5faa4103f
YakumoRan68 <37825675+YakumoRan68@users.noreply.github.com>
2019-04-09 03:03:53 +0900

    0409

---
+++ Algorithm/star.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+int abs(int x) {
+	return x > 0 ? x : -x;
+}
+
+//                  조건
+// 1. for문은 2개만(바깥 for문, 안쪽 for문)
+// 2. 바깥쪽 for문은 무조건 i++로만
+// 3. 안쪽 for문의 조건문은 무조건 1줄.
+// 즉 구성이 for - for - if 만 있는 구조로만 구성됨.
+// 이론상 표현하려는 것에 패턴이 있고, 2차원 평면 모양이면 for문 2개로 가능하다는 것을 증명.
+
+void main() {
+	int MAX = 3;
+	//scanf_s("%d", &MAX);
+
+	printf("1\n");
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) 
+			i > j - 1 ? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n2\n");
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) 
+			i < MAX - j ? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n3\n");
+
+	for (int i = 0; i < MAX; i++) { 
+		for (int j = 0; j < MAX; j++) 
+			i < MAX - j - 1 ? printf(" ") : printf("*");
+		printf("\n");
+	}
+
+	printf("\n4\n");
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) 
+			i > j ? printf(" ") : printf("*");
+		printf("\n");
+	}
+
+	printf("\n5\n");
+
+	for (int i = 0; i < 2 * MAX - 1; i++) {
+		for (int j = 0; j < MAX; j++)
+			abs(MAX - i - 1) < MAX - j ? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n6\n");
+
+	for (int i = 0; i < 2 * MAX - 1; i++) {
+		for (int j = 0; j < MAX; j++)
+			abs(MAX - i - 1) < j + 1? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n7\n");
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX * 2 - 1; j++)
+			i < MAX - abs(MAX - j - 1) ? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n8\n");
+
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX * 2 - 1; j++)
+			i < abs(MAX - j - 1) ? printf(" ") : printf("*");
+		printf("\n");
+	}
+
+	printf("\n9\n");
+
+	for (int i = 0; i < MAX * 2 - 1; i++) {
+		for (int j = 0; j < MAX * 2 - 1; j++)
+			abs(MAX - i - 1) < MAX - abs(MAX - j - 1) ? printf("*") : printf(" ");
+		printf("\n");
+	}
+
+	printf("\n10\n");
+
+	for (int i = 0; i < MAX * 2 - 1; i++) {
+		for (int j = 0; j < MAX * 2 - 1; j++)
+			abs(MAX - i - 1) < abs(MAX - j - 1) ? printf(" ") : printf("*");
+		printf("\n");
+	}
+
+	printf("\nwindmill\n");
+
+	for (int i = 0; i < MAX * 2; i++) {
+		for (int j = 0; j < MAX * 2; j++)
+			abs(MAX - abs(i - MAX + 1)) % (MAX + 1) - 1 < j ? printf("*") : printf(" ");
+		printf("\n");
+	}
+}
