d9bfcedf78c89c28d9a297dd0ed93d035490f2d8
YakumoRan68 <37825675+YakumoRan68@users.noreply.github.com>
2019-04-14 22:53:43 +0900

    최종

--- Algorithm/star.c
+++ Algorithm/star.c
@@ -4,14 +4,9 @@ int abs(int x) {
 	return x > 0 ? x : -x;
 }
 
-//i < (MAX + 1) * MAX + 1 (+1은 \n을 의미); 가로 * 세로 + 1 
-//i % 가로
-//i = i / 가로
-//j = i % (가로 - 1) (-1 은 행에서 \n을 생각하지 않고 계산하기 위해서) 
-
 void main() {
-	int MAX = 3;
-	//scanf_s("%d", &MAX);
+	int MAX;
+	scanf_s("%d", &MAX);
 
 	printf("\n1");
 
@@ -55,20 +50,5 @@ void main() {
 
 	printf("\nwindmill");
 
-	//1234321
-	//1230321
-	//***   * (|y| % 4 >= 1, <= 1)
-	// **  ** (|y|     >= 2, <= 2)
-	//  * *** (|y|     >= 3, <= 3)
-	//        (|y|     >= 4, <= 4)
-	//  * *** (|y|     >= 3, <= 3)
-	// **  ** (|y|     >= 2, <= 2)
-	//***   * (|y|     >= 1, <= 1)
-
-	//(j < MAX ? MAX - abs(MAX - i) < j % (MAX + 1) + 1 : 
-	//MAX - abs(MAX - i) + 1 > MAX - abs(MAX - j) % (MAX + 1) && i != MAX) ? printf("*") : printf(" ");
-	for (int i = 0; i < (MAX * 2 + 2) * (MAX * 2 + 1) + 1; i++)
-		i % (MAX * 2 + 2) ?
-		(i % (MAX * 2 + 2) -1 < MAX ? MAX - abs(MAX - i / (MAX * 2 + 2)) < i % (MAX * 2 + 2) % (MAX + 1) 
-		: MAX - abs(MAX - i / (MAX * 2 + 2)) + 1 > MAX - abs(MAX - i % (MAX * 2 + 2) + 1) % (MAX + 1) && i / (MAX * 2 + 2) != MAX) ? printf("*") : printf(" ") : printf("\n");
+	for (int i = 0; i < (MAX * 2 + 2) * (MAX * 2 + 1) + 1; i++) i % (MAX * 2 + 2) ? (i % (MAX * 2 + 2) -1 < MAX ? MAX - abs(MAX - i / (MAX * 2 + 2)) < i % (MAX * 2 + 2) % (MAX + 1) : MAX - abs(MAX - i / (MAX * 2 + 2)) + 1 > MAX - abs(MAX - i % (MAX * 2 + 2) + 1) % (MAX + 1) && i / (MAX * 2 + 2) != MAX) ? printf("*") : printf(" ") : printf("\n");
 }
